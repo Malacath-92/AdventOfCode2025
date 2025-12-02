1e2421b4d79a231f2dc594e5bd66825bcef8443d
Hanno <vingine.dev@gmail.com>
2025-12-02 14:03:29 +0100

    Day 2 Part 2

--- 2025_2.py
+++ 2025_2.py
@@ -2,6 +2,7 @@ import aocd
 
 import cli
 
+import functools
 import itertools
 
 sample_data = """\
@@ -9,6 +10,12 @@ sample_data = """\
 data = sample_data if cli.sample else aocd.data
 
 id_ranges = [tuple(id_range.split("-")) for id_range in data.split(",")]
+all_ids = list(
+    itertools.chain.from_iterable(
+        [range(int(start), int(end) + 1) for start, end in id_ranges]
+    )
+)
+
 
 def is_invalid_id(id):
     id_str = str(id)
@@ -17,11 +24,33 @@ def is_invalid_id(id):
         return id_str[:half_len] == id_str[half_len:]
     return False
 
-all_ids = list(
-    itertools.chain.from_iterable(
-        [range(int(start), int(end) + 1) for start, end in id_ranges]
-    )
-)
+
 invalid_ids = [id for id in all_ids if is_invalid_id(id)]
 
 print(f"Problem 1: {sum(invalid_ids)}")
+
+
+@functools.lru_cache
+def divisors(n):
+    def divisors_impl(n):
+        yield n
+        for i in range(2, int(n // 2 + 1)):
+            if n % i == 0:
+                yield int(i)
+    return tuple(divisors_impl(n))
+
+
+def is_invalid_id(id):
+    id_str = str(id)
+    if len(id_str) < 2:
+        return False
+    for div in divisors(len(id_str)):
+        frac_len = len(id_str) // div
+        if id_str == id_str[:frac_len] * div:
+            return True
+    return False
+
+
+invalid_ids = [id for id in all_ids if is_invalid_id(id)]
+
+print(f"Problem 2: {sum(invalid_ids)}")
