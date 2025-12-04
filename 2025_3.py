b3f3dbf7e747ecdc528de347403efc087e490bc5
Hanno <vingine.dev@gmail.com>
2025-12-04 10:48:53 +0100

    Day 3 Part 2

--- 2025_3.py
+++ 2025_3.py
@@ -14,22 +14,35 @@ data = sample_data if cli.sample else aocd.data
 
 banks = [list(map(int, bank)) for bank in data.splitlines()]
 
-def max_joltage(bank):
-    num_batteries = len(bank)
+
+def max_joltage(bank, joltage_size):
+    def next_battery(bank):
+        num_batteries = len(bank)
+
+        next_battery_index = 0
+        for i in range(1, num_batteries):
+            battery = bank[i]
+            if battery > bank[next_battery_index]:
+                next_battery_index = i
+
+        return next_battery_index
+
     max_joltage = 0
-    for i in range(num_batteries):
-        left_battery = bank[i] * 10
-        if max_joltage % 10 >= left_battery:
-            continue
-
-        for j in range(i + 1, num_batteries):
-            right_battery = bank[j]
-            joltage = left_battery + right_battery
-            max_joltage = max(max_joltage, joltage)
-
+    used_bank = 0
+    for i in range(joltage_size):
+        remaining = joltage_size - i - 1
+        available_bank = bank[used_bank:-remaining] if remaining else bank[used_bank:]
+        next_battery_index = next_battery(available_bank)
+        max_joltage = max_joltage * 10 + available_bank[next_battery_index]
+        used_bank += next_battery_index + 1
     return max_joltage
 
 
-joltages = [max_joltage(bank) for bank in banks]
+joltages = [max_joltage(bank, 2) for bank in banks]
 
 print(f"Problem 1: {sum(joltages)}")
+
+
+joltages = [max_joltage(bank, 12) for bank in banks]
+
+print(f"Problem 2: {sum(joltages)}")
