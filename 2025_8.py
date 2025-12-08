a0217a3b2281abe98215bcd272bc441336139370
Hanno <vingine.dev@gmail.com>
2025-12-08 07:21:16 +0100

    Day 8 Part 1

---
+++ 2025_8.py
@@ -0,0 +1,68 @@
+import aocd
+
+import cli
+
+from vector import Vector as Vector
+
+from itertools import combinations
+from functools import reduce
+from collections import defaultdict
+
+sample_data = """\
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689"""
+data = sample_data if cli.sample else aocd.data
+total_connections = 10 if cli.sample else 1000
+
+boxes = [Vector(*map(int, line.split(","))) for line in data.splitlines()]
+
+connections = list(combinations(boxes, r=2))
+connections_with_distances = [
+    ((lhs - rhs).len_sqr, (lhs, rhs)) for lhs, rhs in connections
+]
+connections_with_distances_sorted = sorted(
+    connections_with_distances, key=lambda x: x[0]
+)
+connections_sorted = list(map(lambda x: x[1], connections_with_distances_sorted))
+
+circuits = {}
+for lhs, rhs in connections_sorted[:total_connections]:
+    if lhs not in circuits:
+        circuits[lhs] = len(circuits)
+    lhs_circuit = circuits[lhs]
+
+    if rhs not in circuits:
+        circuits[rhs] = lhs_circuit
+        continue
+    rhs_circuit = circuits[rhs]
+
+    if lhs_circuit != rhs_circuit:
+        for box, circuit_id in circuits.items():
+            if circuit_id == rhs_circuit:
+                circuits[box] = lhs_circuit
+
+circuit_sizes = defaultdict(lambda: 0)
+for circuit_id in circuits.values():
+    circuit_sizes[circuit_id] += 1
+circuit_sizes = list(circuit_sizes.values())
+biggest_circuits = sorted(circuit_sizes, reverse=True)[:3]
+
+print("Part 1:", reduce(lambda a, b: a * b, biggest_circuits))
--- vector.py
+++ vector.py
@@ -2,33 +2,75 @@ import math
 from typing import NamedTuple
 
 
-class Vector(NamedTuple):
+class Vector2(NamedTuple):
     x: int
     y: int
 
     def __add__(lhs, rhs):
-        return Vector(lhs.x + rhs.x, lhs.y + rhs.y)
+        return Vector2(lhs.x + rhs.x, lhs.y + rhs.y)
 
     def __sub__(lhs, rhs):
-        return Vector(lhs.x - rhs.x, lhs.y - rhs.y)
+        return Vector2(lhs.x - rhs.x, lhs.y - rhs.y)
 
     def __mul__(lhs, rhs: int):
-        return Vector(lhs.x * rhs, lhs.y * rhs)
+        return Vector2(lhs.x * rhs, lhs.y * rhs)
 
     def __truediv__(lhs, rhs: int):
-        return Vector(lhs.x / rhs, lhs.y / rhs)
+        return Vector2(lhs.x / rhs, lhs.y / rhs)
 
+    @property
     def len(self):
-        return math.sqrt(self.x * self.x + self.y * self.y)
+        return math.sqrt(self.len_sqr)
+
+    @property
+    def len_sqr(self):
+        return self.x * self.x + self.y * self.y
 
     def neighbours(self):
         return [
-            self + Vector(+1, +0),
-            self + Vector(-1, +0),
-            self + Vector(+0, +1),
-            self + Vector(+0, -1),
-            self + Vector(+1, +1),
-            self + Vector(-1, -1),
-            self + Vector(+1, -1),
-            self + Vector(-1, +1),
+            self + Vector2(+1, +0),
+            self + Vector2(-1, +0),
+            self + Vector2(+0, +1),
+            self + Vector2(+0, -1),
+            self + Vector2(+1, +1),
+            self + Vector2(-1, -1),
+            self + Vector2(+1, -1),
+            self + Vector2(-1, +1),
         ]
+
+
+class Vector3(NamedTuple):
+    x: int
+    y: int
+    z: int
+
+    def __add__(lhs, rhs):
+        return Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
+
+    def __sub__(lhs, rhs):
+        return Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z)
+
+    def __mul__(lhs, rhs: int):
+        return Vector3(lhs.x * rhs, lhs.y - rhs.y, lhs.z * rhs.z)
+
+    def __truediv__(lhs, rhs: int):
+        return Vector3(lhs.x / rhs, lhs.y / rhs.y, lhs.z / rhs.z)
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+
+    @property
+    def len(self):
+        return math.sqrt(self.len_sqr)
+
+    @property
+    def len_sqr(self):
+        return self.x * self.x + self.y * self.y + self.z * self.z
+
+def Vector(*args):
+    if len(args) == 2:
+        return Vector2(args[0], args[1])
+    elif len(args) == 3:
+        return Vector3(args[0], args[1], args[2])
+    else:
+        raise ValueError("Vector must be initialized with 2 or 3 arguments.")
